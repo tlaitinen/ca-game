b5edc286146377e4bed8a4150aba2edb0d8c7cc4
Tero Laitinen <tero.laitinen@wolt.com>
2019-12-28 19:33:30 +0200

    socket.io test ok

--- src/server.ts
+++ src/server.ts
@@ -4,18 +4,24 @@ import http from 'http';
 import socketIo from 'socket.io';
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(http);
+const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 const port = parseInt(process.env.PORT || '5000');
+io.on('connection', socket => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', function() {
+    console.log('user disconnected', socket.id);
+  });
+  socket.on('fuu', msg => {
+    console.log(msg);
+    io.emit('fuu', msg);
+  });
+});
 server.listen(port, () => {
   console.log('listening on *:' + port);
-});
-
-io.on('connection', socket => {
-  console.log('a user connected', socket);
 });
